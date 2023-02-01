5f30a4dd691067bf30db68a0e9bc36959bc6f0dc
Khoi <khoi.ntm22707@sinhvien.hoasen.edu.vn>
2023-02-01 15:50:44 +0700

    D02_Logindemo

---
+++ file.js
@@ -0,0 +1,24 @@
+
+
+let secrectnumber =  Math.floor(Math.random() * 100);
+
+
+ while(true){
+let input = prompt("Nhap so bi mat")
+input = parseInt(input);
+if(input<secrectnumber){
+    alert("So phai lon hon ");
+    break;}
+else{
+    alert("So phai nho hon")
+}
+
+
+ }
+
+ 
+
+
+
+
+
